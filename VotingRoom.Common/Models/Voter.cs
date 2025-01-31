85ebc8373d4e6c36f27a6c37a9c334f25fd32b4b
tomjonesdev <tomjones_22@hotmail.com>
2025-01-31 17:51:56 +0000

    Add proper voting and UI behaviour.

---
+++ VotingRoom.Common/Models/Voter.cs
@@ -0,0 +1,8 @@
+﻿namespace VotingRoom.Common.Models;
+
+public class Voter
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+}
--- VotingRoom/VotingRoom/Hubs/VoteHub.cs
+++ VotingRoom/VotingRoom/Hubs/VoteHub.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using VotingRoom.Common.Models;
 
 namespace VotingRoom.Hubs;
 
 public class VoteHub : Hub
 {
-    public async Task Vote(string user, int vote)
+    public async Task Vote(Voter voter, int voteItemId)
     {
-        await Clients.All.SendAsync("ReceiveVote", user, vote);
+        await Clients.All.SendAsync("ReceiveVote", voter.Id, voter.Name, voteItemId);
     }
 }
