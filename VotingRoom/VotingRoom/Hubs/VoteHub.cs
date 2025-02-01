812de91a0107129c9e3679b6abd9e77ac18bc530
tomjonesdev <tomjones_22@hotmail.com>
2025-02-01 08:42:31 +0000

    Add participant list and join logic.

--- VotingRoom/VotingRoom/Hubs/VoteHub.cs
+++ VotingRoom/VotingRoom/Hubs/VoteHub.cs
@@ -5,8 +5,25 @@ namespace VotingRoom.Hubs;
 
 public class VoteHub : Hub
 {
+    public override Task OnConnectedAsync()
+    {
+        // TODO: Implement for joining behaviour?
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        // TODO: Implement for leaving behaviour?
+        return base.OnDisconnectedAsync(exception);
+    }
+
     public async Task Vote(Voter voter, int voteItemId)
     {
         await Clients.All.SendAsync("ReceiveVote", voter.Id, voter.Name, voteItemId);
+    }
+
+    public async Task VoterJoin(Voter voter)
+    {
+        await Clients.All.SendAsync("VoterJoin", voter);
     }
 }
